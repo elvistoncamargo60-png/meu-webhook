acc9af9b6d21abc9ffcb1d2266df3d579dbcc3be
elvistoncamargo60-png <elvistoncamargo60@gmail.com>
2026-05-10 19:15:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,3 @@ def webhook():
 Preço: {preco}
 Link afiliado: {link}"
     }), 200
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
